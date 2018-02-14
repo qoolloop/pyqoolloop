c552a527a5d46b03ac7c43639bc537888be48bd9
Kan Torii <kan@qoolloop.com>
2018-02-14 20:22:16 +0900

    Add AttributeHolder class.

---
+++ object.py
@@ -0,0 +1,11 @@
+
+
+class AttributeHolder(object):
+    """
+    A class to which you can add attributes at will
+
+    E.g.:
+      instance = AttributeHolder()
+      instance.attr = value
+    """
+    pass
