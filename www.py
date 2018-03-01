eb5407b31e461ebe65db76e7e8f7da300e2938ca
Kan Torii <kan@qoolloop.com>
2018-03-01 16:16:23 +0900

    Add comment.

--- www.py
+++ www.py
@@ -1,7 +1,7 @@
 
 
 def check_url(url):
-    return url.find('"') < 0  #TODO: better validation
+    return url.find('"') < 0  #TODO: better validation. It might be best to make sure the path points to an endpoint
     # http://homakov.blogspot.jp/2014/01/evolution-of-open-redirect-vulnerability.html
     # http://validators.readthedocs.io/en/latest/#module-validators.url
     # https://stackoverflow.com/questions/35149861/equivalent-urllib-parse-quote-in-python-2-7
