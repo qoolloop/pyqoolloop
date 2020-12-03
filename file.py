7dc06ece199eb729437f0bcfe05eb0b0cfe95ed4
Kan Torii <kan@qoolloop.com>
2020-12-03 09:28:57 +0900

    Add docstrings for `file.py`.

--- file.py
+++ file.py
@@ -1,20 +1,41 @@
 """
-Note that reading pickle files from an unknown source can be a security risk.
+.. note::
+  Note that reading pickle files from an unknown source can be a security risk.
 """
 import os
 import pickle
+from typing import (
+    Iterable,
+    Optional,
+)
 
 
-def get_file_path(filename):
+#TODO: rename `get_directory()`
+def get_file_path(filename: str) -> str:  #TODO: rename `path`
     """
-    Argument:
-    filename -- Specify __file__
+    Get directory of file.
+
+    :param filename: Path to file. Specify `__file__`, for example.
+    :return: Path to directory of the file.
     """
     file_path = os.path.dirname(os.path.abspath(filename))
     return file_path
     
 
-def load_pickle(file_path, filename, raise_exception=False):
+def load_pickle(
+        file_path: str, filename: str, raise_exception: bool = False
+) -> Optional[object]:
+    """
+    Load from pickle file.
+
+    :param file_path: Path to directory.
+    :param filename: Name of file in `file_path` directory.  #TODO: Accept `None` to specify file with `file_path`?
+    :param raise_exception: `True` to raise exception on error, such as
+      file not found. If `False`, this function will return `None` on error.
+
+    :raise FileNotFoundError: File is not found
+    :raise other: Other file related errors.
+    """
     try:
         with open(os.path.join(file_path, filename), 'rb') as f:
             value = pickle.load(f)
@@ -39,7 +60,18 @@ def _check_overwrite(full_filename, overwrite):
     # endif
     
 
-def dump_pickle(file_path, filename, value, overwrite=False):
+def dump_pickle(
+        file_path: str, filename: str, value: object, overwrite: bool = False):
+    """
+    Save to pickle file.
+
+    :param file_path: Path to directory.
+    :param filename: Name of file in `file_path` directory.
+    :param overwrite: `True` to write over existing file.
+
+    :raises FileExistsError: Raised when file already exists and
+      `overwrite` is `False`
+    """
     full_filename = os.path.join(file_path, filename)
 
     _check_overwrite(full_filename, overwrite)
@@ -49,7 +81,23 @@ def dump_pickle(file_path, filename, value, overwrite=False):
     # endwith
 
 
-def load_text(file_path, filename, raise_exception=False):
+def load_text(
+    file_path: str, filename: str, raise_exception: bool = False
+) -> Optional[str]:
+    """
+    Read text from file.
+
+    :param file_path: Path to directory.
+    :param filename: Name of file in `file_path` directory.  #TODO: Accept `None` to specify file with `file_path`?
+    :param raise_exception: `True` to raise exception on error, such as
+      file not found. If `False`, this function will return `None` on error.
+
+    :raise FileNotFoundError: File is not found
+    :raise other: Other file related errors.
+
+    :return: Text read from file. `None` if file doesn't exist and
+      `file_`raise_exception` is `False`.
+    """
     try:
         with open(os.path.join(file_path, filename), 'r') as f:
             return f.read()
@@ -66,6 +114,16 @@ def load_text(file_path, filename, raise_exception=False):
 
 
 def dump_text(file_path, filename, value, overwrite=False):
+    """
+    Save text to file.
+
+    :param file_path: Path to directory.
+    :param filename: Name of file in `file_path`.
+    :param overwrite: `True` to write over existing file.
+
+    :raises FileExistsError: Raised when file already exists and
+      `overwrite` is `False`
+    """
     full_filename = os.path.join(file_path, filename)
 
     _check_overwrite(full_filename, overwrite)
@@ -75,7 +133,15 @@ def dump_text(file_path, filename, value, overwrite=False):
     # endwith
 
 
-def list_directories(path, exclusion=()):
+def list_directories(
+        path: str, exclusion: Iterable[str] = ()) -> Optional[str]:
+    """
+    List directories in directory.
+    
+
+    :param path: Path to directory.
+    :param exclusion: Directories to ignore.
+    """
     exclusion = set(exclusion)
     
     result = []
