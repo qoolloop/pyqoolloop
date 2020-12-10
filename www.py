5c0ec4a80c79396b99d29215605b26ecc37b18a6
Kan Torii <kan@qoolloop.com>
2020-12-10 15:19:05 +0900

    Docstring for `www.py`.

--- www.py
+++ www.py
@@ -1,6 +1,17 @@
+"""
+Module for web related functions.
+"""
 
+def check_url(url: str) -> bool:
+    """
+    Check whether URL is properly formed.
 
-def check_url(url):
+    :param url: String to be checked.
+
+    :returns: `True`, if `url` is properly formed as a URL.
+
+    #TODO: Still under development
+    """
     return url.find('"') < 0  #TODO: better validation. It might be best to make sure the path points to an endpoint
     # http://homakov.blogspot.jp/2014/01/evolution-of-open-redirect-vulnerability.html
     # http://validators.readthedocs.io/en/latest/#module-validators.url
