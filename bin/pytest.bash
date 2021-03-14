a4e84724b7fd0ce7635c601b212217bc97e5e8af
Kan Torii <kan@qoolloop.com>
2021-03-14 12:02:12 +0900

    Setup pylint.

---
+++ bin/pytest.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+options="$1"
+
+this_folder=$(dirname $(readlink -f "${BASH_SOURCE[-1]}"))
+parent_folder=$(dirname ${this_folder})
+
+py.test --cov="$parent_folder" --mypy --pylint --ignore="${parent_folder}/docs" ${options}
+
+# https://github.com/pytest-dev/pytest/issues/913#issuecomment-387969212
+exit $?
