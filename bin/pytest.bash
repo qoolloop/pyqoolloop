59c1e876058e12b07ec518368ab82c4a54f2657f
Kan Torii <kan@qoolloop.com>
2021-06-05 19:14:01 +0900

    Move `py.test` options to `pytest.ini`.

--- bin/pytest.bash
+++ bin/pytest.bash
@@ -5,7 +5,8 @@ options="$1"
 this_folder=$(dirname $(readlink -f "${BASH_SOURCE[-1]}"))
 parent_folder=$(dirname ${this_folder})
 
-py.test --cov="$parent_folder" --mypy --pylint --pydocstyle --doctest-modules --ignore="${parent_folder}/docs" ${options}
+# options in pytest.ini
+py.test --cov="$parent_folder" --ignore="${parent_folder}/docs" ${options}
 
 # https://github.com/pytest-dev/pytest/issues/913#issuecomment-387969212
 exit $?
