2ce64d0e09e30c2a16dc1f00d5fa085e5f964aef
Kan Torii <oss@qoolloop.com>
2021-12-22 13:01:30 +0900

    Add `requirement.txt` file.

--- testutils.py
+++ testutils.py
@@ -219,6 +219,15 @@ def combine_lists(
     If one of the arguments is not iterable, it will be treated as though
     it was in a list.
 
+    If both elements in the arguments are iterable, the result will be a
+    concatenation.
+
+    If one of the elements is iterable and the other is not, the non-iterable
+    will be prefixed or appended to the iterable.
+
+    If both elmenets in the arguments are not iterable, a list will be created
+    with both elments.
+
       >>> to_set( combine_lists(('a', 'b'), ('c', 'd')) ) == \
       ...  to_set( [['a', 'c'], ['a', 'd'], ['b', 'c'], ['b', 'd']] )
       True
